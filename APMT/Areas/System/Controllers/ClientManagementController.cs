808f4dfd93be538417e739418eee060de478c9d5
Tomoki <Tomoki@NATSUME>
2017-03-23 15:43:35 +0700

    load view list in client

--- APMT/Areas/System/Controllers/ClientManagementController.cs
+++ APMT/Areas/System/Controllers/ClientManagementController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Models;
 
 namespace APMT.Areas.System.Controllers
 {
     public class ClientManagementController : Controller
     {
+        CP_SPMEntities1 db = new CP_SPMEntities1();
         // GET: System/Client
         public ActionResult View_List()
         {
-            return View();
+            var lstCompany = db.APMT_Company.ToList();
+            return View(lstCompany);
         }
     }
 }
