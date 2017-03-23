88117b8a16a5d137a0bcc3c88ba7df4d09a9c2fe
kimquang1995 <kimquang1995@gmail.com>
2017-03-23 20:08:46 +0700

    complete Add_New,setAdministrator,setCreator,setMember,setStatus,viewInfor function

--- APMT/Areas/Company/Controllers/MemberManagementController.cs
+++ APMT/Areas/Company/Controllers/MemberManagementController.cs
@@ -18,15 +18,20 @@ namespace APMT.Areas.Company.Controllers
                         join user in db.APMT_User on UserC.User_id equals user.ID
                         join company in db.APMT_Company on UserC.Company_id equals company.ID
                         where company.ID == 1
-                        select new userCompany { id = UserC.ID,
+                        select new userCompany
+                        {
+                            id = UserC.ID,
                             fullName = user.Fullname,
                             email = user.Email,
                             avartar = user.Avatar,
                             createAt = user.Create_at.ToString(),
                             updateAt = user.Update_at.ToString(),
                             isAllowed = UserC.Allowed,
-                            isAdministrator = UserC.isAdministrator,isCreator=UserC.isCreator,isMember=UserC.isMember};
-      
+                            isAdministrator = UserC.isAdministrator,
+                            isCreator = UserC.isCreator,
+                            isMember = UserC.isMember
+                        };
+
             ViewBag.List = query.OrderByDescending(x => x.id).ToList();
             return View();
         }
@@ -45,7 +50,7 @@ namespace APMT.Areas.Company.Controllers
             ViewBag.Message = null;
             string trimEmail = "";
             string email = f["somevalue"];
-           // string role = f["selectRole"];
+            // string role = f["selectRole"];
             if (email.Contains("("))
             {
                 trimEmail = email.Substring(0, email.IndexOf('(')).Trim();
@@ -56,26 +61,41 @@ namespace APMT.Areas.Company.Controllers
             }
             try
             {
-                var userID = db.APMT_User.SingleOrDefault(x => x.Email.Equals(trimEmail)).ID;
+                var user = db.APMT_User.SingleOrDefault(x => x.Email.Equals(trimEmail));
+                var userID = user.ID;
                 if (userID != null)
-                {                  
-                    APMT_Company_User companyUser = new APMT_Company_User();
-                    companyUser.Company_id = 1;
-                    companyUser.User_id = int.Parse(userID.ToString());
-                    companyUser.Allowed = 1;
-                    companyUser.isMember = true;
-                    companyUser.isAdministrator = false;
-                    companyUser.isCreator = false;
-                    db.APMT_Company_User.Add(companyUser);
-                    db.SaveChanges();
-                    TempData["Message"] = "Successful";
-                        return RedirectToAction("View_List");                  
-                }
-                else
                 {
-                    TempData["Message"] = "User is not exist !";
-                    return RedirectToAction("View_List");
+                    if (user.Allowed == 1)
+                    {
+                        var existMember = db.APMT_Company_User.SingleOrDefault(x => x.User_id == userID);
+                        if (existMember == null)
+                        {
+                            APMT_Company_User companyUser = new APMT_Company_User();
+                            companyUser.Company_id = 1;
+                            companyUser.User_id = int.Parse(userID.ToString());
+                            companyUser.Allowed = 1;
+                            companyUser.isMember = true;
+                            companyUser.isAdministrator = false;
+                            companyUser.isCreator = false;
+                            db.APMT_Company_User.Add(companyUser);
+                            db.SaveChanges();
+                            TempData["Message"] = "Successful";
+                            return RedirectToAction("View_List");
+                        }
+                        else
+                        {
+                            TempData["Message"] = "User is not exist !";
+                            return RedirectToAction("View_List");
+                        }
+                        
+                    }
+                    else
+                    {
+                        TempData["Message"] = "This user was Blocked";
+                        return RedirectToAction("View_List");
+                    }
                 }
+                return RedirectToAction("View_List");
             }
             catch (Exception e)
             {
@@ -94,9 +114,10 @@ namespace APMT.Areas.Company.Controllers
         public ActionResult setAdministrator(int? id)
         {
             var user = db.APMT_Company_User.FirstOrDefault(x => x.ID == id);
-            if(user.isAdministrator == false)
-            { 
-            user.isAdministrator = true;
+            if (user.isAdministrator == false)
+            {
+                user.isAdministrator = true;
+                user.isMember = false;
             }
             else
             {
@@ -105,6 +126,7 @@ namespace APMT.Areas.Company.Controllers
             db.Entry(user).State = EntityState.Modified;
             db.SaveChanges();
             return RedirectToAction("View_List");
+
         }
         public ActionResult setCreator(int? id)
         {
@@ -120,6 +142,7 @@ namespace APMT.Areas.Company.Controllers
             db.Entry(user).State = EntityState.Modified;
             db.SaveChanges();
             return RedirectToAction("View_List");
+
         }
         public ActionResult setMember(int? id)
         {
@@ -127,6 +150,7 @@ namespace APMT.Areas.Company.Controllers
             if (user.isMember == false)
             {
                 user.isMember = true;
+                user.isAdministrator = false;
             }
             else
             {
@@ -135,6 +159,7 @@ namespace APMT.Areas.Company.Controllers
             db.Entry(user).State = EntityState.Modified;
             db.SaveChanges();
             return RedirectToAction("View_List");
+
         }
         public ActionResult setStatus(int? id)
         {
@@ -151,7 +176,13 @@ namespace APMT.Areas.Company.Controllers
             db.SaveChanges();
             return RedirectToAction("View_List");
         }
-
+        public ActionResult viewInfor(int? id)
+        {
+            var userCompany = db.APMT_Company_User.SingleOrDefault(x => x.ID == id);
+            int? userID = userCompany.User_id;
+            APMT_User user = db.APMT_User.Find(userID);
+            return View(user);
+        }
         //[HttpPost]
         //public ActionResult deleteMember(APMT_Company_User user)
         //{
