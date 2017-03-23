2bed71e2f27046527c536e2d70cd786a148d4b42
MINHLUANPC\Minh Luan <cuminhluan1995>
2017-03-23 17:37:28 +0700

    commit enable/disable client management

--- APMT/Areas/System/Controllers/ClientManagementController.cs
+++ APMT/Areas/System/Controllers/ClientManagementController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Models;
+using System.Data.Entity;
 
 namespace APMT.Areas.System.Controllers
 {
@@ -15,6 +16,23 @@ namespace APMT.Areas.System.Controllers
         {
             var lstCompany = db.APMT_Company.ToList();
             return View(lstCompany);
+        }
+        public ActionResult setStatus(int? id)
+        {
+            var company = db.APMT_Company.FirstOrDefault(x => x.ID == id);
+            if (company.Allowed == true)
+            {
+                company.Allowed = false;
+                company.Update_at = DateTime.Now;
+            }
+            else
+            {
+                company.Allowed = true;
+                company.Update_at = DateTime.Now;
+            }
+            db.Entry(company).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("View_List");
         }
     }
 }
